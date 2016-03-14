4ec8f728b2f13bbfc8f83bcbdf7f699b72389d7b
Nevena Djaja <nevena.djaja@gmail.com>
2016-03-14 13:24:26 -0700

    Added new movie form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "movies#index"
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # get "movies" => "movies#index"
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # get "movies/new" => "movies#new"
+  # patch "movies/:id" => "movies#update"
 
-  patch "movies/:id" => "movies#update"
+  resources :movies
 end
