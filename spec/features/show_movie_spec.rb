724f8a468ad2e5426267a4e0006170b6b8355fdf
Nevena Djaja <nevena.djaja@gmail.com>
2016-03-19 14:24:27 -0700

    added new fields to the movies

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -6,6 +6,11 @@ describe "Viewing an individual movie" do
 		visit movie_url(movie.id)
 
 		expect(page).to have_text(movie.title)
+		expect(page).to have_text(movie.cast)
+		expect(page).to have_text(movie.director)
+		expect(page).to have_text(movie.duration)
+
+		expect(page).to have_selector("img")
 	end
 
 	it "should show the total gross if it exceeds $50 million" do
